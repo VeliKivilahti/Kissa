8d76df197324bc2780e0beaf4d70e4d60466990b
Veli Kivilahti <veli.kivilahti@gmail.com>
2021-05-03 18:44:48 +0300

    Redid the program to have all frames in same file

--- Main.py
+++ Main.py
@@ -1,30 +1,44 @@
-from afterLogin import AfterLogin
 import tkinter as tk 
-from login import Login
+
+def loginToSecond():
+    login.pack_forget()
+    nextFrame.pack()
+
+def secondToLogin():
+    nextFrame.pack_forget()
+    login.pack()
+#Init Tkinter
+root = tk.Tk()
+
+#Login frame
+login = tk.Frame(root)
+lbl_name=tk.Label(login,text="name")
+lbl_pw=tk.Label(login,text="password")
+namebar=tk.Text(login,width=25,height=1)
+pwbar=tk.Text(login,width=25,height=1)
+btn_ok=tk.Button(login,text="OK",
+                command= loginToSecond)
+cat=tk.Label(login,text="😺",font=("Courier",64))
+
+cat.grid(row=0,column=0)
+lbl_name.grid(row=1, column=0)
+namebar.grid(row=2, column=0)
+lbl_pw.grid(row=3, column=0)
+pwbar.grid(row=4, column=0)
+btn_ok.grid(row=5, column=0)
+
+#Second frame
+nextFrame =tk.Frame(root)
+kissa2=tk.Label(nextFrame,text="😺",font=("Courier",200))
+btn_back=tk.Button(nextFrame,text="back",command=secondToLogin)
+
+kissa2.grid(row=0,column=0)
+btn_back.grid(row=1,column=0)
+
+login.pack()
 
 
 
-class MainView(tk.Frame):
-    def __init__(self, *args, **kwargs):
-        tk.Frame.__init__(self, *args, **kwargs)       
-        p1 = Login(self)
-        p2 = AfterLogin(self)
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
 
 
-        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p2.show()
-        p1.show()
-        
-
-
-
-if __name__ == "__main__":
-    window = tk.Tk()
-    main =  MainView(window)
-    main.pack(side="top", fill="both", expand=True)
-    window.wm_geometry("300x600")
-    
-    window.mainloop()
+root.mainloop()
--- afterLogin.py
+++
@@ -1,9 +0,0 @@
-from page import Page
-import tkinter as tk
-
-class AfterLogin(Page):
-     def __init__(self, *args, **kwargs):
-        
-        Page.__init__(self, *args, **kwargs)
-        lbl_name=tk.Label(self,text="😺",font=("Courier",128))
-        lbl_name.grid(row=0,column=0)
