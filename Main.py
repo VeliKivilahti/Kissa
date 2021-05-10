fdcdaf21c622c06373e07811b9976860b4ab9385
TuomasMattila <tuomas.mattila@windowslive.com>
2021-05-10 21:06:50 +0300

    Improvements to the look of the profile page.

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+from tkinter.messagebox import showinfo
 from tkinter import filedialog
 email = "sahkoposti"
 password = "salasana"
@@ -59,6 +60,7 @@ root = tk.Tk()
 root.title("Koulu appi")
 root.wm_geometry("360x640")
 
+
 #Login frame
 frm_login = ttk.Frame(root)
 frm_login.columnconfigure(0, weight=1)
@@ -79,6 +81,7 @@ lbl_pw.grid(row=4, column=0, sticky="w", padx=40)
 ent_pw.grid(row=5, column=0, padx=40)
 btn_ok.grid(row=6, column=0, pady=30)
 
+
 #Greeting frame
 frm_greeting = ttk.Frame(root)
 img_speachBubble = tk.PhotoImage(file="pictures/speachBubble.png")
@@ -98,13 +101,14 @@ frm_buttons.grid(row=3, column=0, pady=20)
 btn_back.grid(row=0, column=0, padx=5)
 btn_ok.grid(row=0, column=1, padx=5)
 
+
 #Start page
 frm_main = ttk.Frame(root)
 lbl_name = ttk.Label(frm_main,text="Nimi\nKoulu", font=('Lato', 25))
 img_profile = tk.PhotoImage(file="pictures/profile.png")
 btn_profile = tk.Button(frm_main, image=img_profile, highlightthickness=0, bd=0, command=mainToProfile)
 lbl_laksyt = ttk.Label(frm_main, text="Läksyt", font=('Lato', 25))
-lst_laksyt = tk.Listbox(frm_main, height=9, width=15, font=('Lato', 20), relief=tk.SOLID, bd=2)
+lst_laksyt = tk.Listbox(frm_main, height=9, width=15, font=('Lato', 20), relief=tk.SOLID, bd=1.5)
 list_items = ["Matematiikka 1","Matematiikka 2","Matematiikka 3","Englanti 1","Englanti 2","Historia 1","Historia 2","Äidinkieli 1","Äidinkieli 2","Äidinkieli 3","Äidinkieli 4","Kuvataide 1","Kuvataide 2","Uskonto 1","Uskonto 2","Maantieto 1","Maantieto 2","Biologia 1","Biologia 2","Fysiikka 1","Fysiikka 2","Kemia 1","Kemia 2","Ruotsi 1","Ruotsi 2","Ruotsi 3","Liikunta 1"]
 for item in list_items:
     lst_laksyt.insert("end", item)
@@ -124,6 +128,7 @@ img_schedule =tk.PhotoImage(file="pictures/schedule.png")
 btn_schedule= tk.Button(frm_buttons, image=img_schedule, highlightthickness = 0, bd = 0)
 img_note =tk.PhotoImage(file="pictures/note.png")
 btn_note= tk.Button(frm_buttons, image=img_note, highlightthickness = 0, bd = 0)
+
 lbl_name.grid(row=0, column=0, sticky="NW", pady=20)
 btn_profile.grid(row=0, column=1, sticky="NSEW", pady=20)
 lbl_laksyt.grid(row=2, column=0, sticky="SW")
@@ -135,6 +140,7 @@ frm_buttons.grid(row=4, column=0, columnspan=2, pady=35, sticky="EW")
 btn_mail.grid(row=0, column=0, sticky="W")
 btn_schedule.grid(row=0, column=1)
 btn_note.grid(row=0,column=2, sticky="E")
+
 
 ## Homework Page
 
@@ -148,18 +154,17 @@ def openFile():
     btn_file.grid_forget()
     btn_camera.grid_forget()
     lbl_palautettu.grid(row=5)
-
-    
+ 
 frm_homework = ttk.Frame(root)
 lbl_nameHomework = ttk.Label(frm_homework, text="Nimi\nKoulu", font=('Lato', 25))
 btn_profile = tk.Button(frm_homework,image=img_profile, highlightthickness=0, bd=0,command=homeworkToProfile)
 lbl_ainelaksyt = ttk.Label(frm_homework, text="Läksyt", font=('Lato', 25))
-txt_ainelaksyt = tk.Text(frm_homework,height=9, width=12, font=('Lato', 20),bd=2,relief=tk.SOLID)
-txt_ainelaksyt.insert("end","asd")
+txt_ainelaksyt = tk.Text(frm_homework,height=9, width=15, font=('Lato', 15), bd=1.5, relief=tk.SOLID)
+txt_ainelaksyt.insert("end","Tämä on tehtävänanto oppilaalle.")
 txt_ainelaksyt.configure(state="disabled")
 btn_scrollup = tk.Button(frm_homework, image=img_arrowUp, highlightthickness=0, bd=0,command=lambda:txt_ainelaksyt.yview_scroll(-1,"units"))
 btn_scrolldown =tk.Button(frm_homework, image=img_arrowDown, highlightthickness=0, bd=0,command=lambda:txt_ainelaksyt.yview_scroll(1,"units"))
-lbl_palautus = ttk.Label(frm_homework,text="Palauta viim. 1.4", font=('Lato', 20))
+lbl_palautus = ttk.Label(frm_homework,text="Palauta viim. 1.4", font=('Lato', 18))
 img_palautus= tk.PhotoImage(file="pictures/download.png")
 btn_palautus = tk.Button(frm_homework, image=img_palautus, highlightthickness = 0, bd = 0, command=changeButton)
 img_file=tk.PhotoImage(file="pictures/file.png")
@@ -168,7 +173,7 @@ img_camera=tk.PhotoImage(file="pictures/camera.png")
 btn_camera = ttk.Button (frm_homework, image=img_camera)
 lbl_palautettu = ttk.Label(frm_homework,text = "✓ palautettu",background="green",font=('Lato', 20))
 img_back=tk.PhotoImage(file="pictures/back.png")
-btn_returnToMain = tk.Button(frm_homework,image=img_back,command=homeworkToMain)
+btn_returnToMain = tk.Button(frm_homework, image=img_back, highlightthickness = 0, bd = 0, command=homeworkToMain)
 
 lbl_nameHomework.grid(row=0,column=0,sticky="NW", pady=20)
 btn_profile.grid(row=0, column=1,sticky="NSEW", pady=20)
@@ -176,35 +181,40 @@ lbl_ainelaksyt.grid(row=1,column=0,sticky="SW")
 txt_ainelaksyt.grid(row=2,column=0,ipadx=20)
 btn_scrollup.grid(row=2, column=1, sticky="NW", padx=5)
 btn_scrolldown.grid(row=2, column=1, sticky="SW", padx=5)
-lbl_palautus.grid(row=4) 
+lbl_palautus.grid(row=4, sticky="W") 
 btn_palautus.grid(row=5)
-btn_returnToMain.grid(row=6,sticky="SW",pady=20)
+btn_returnToMain.grid(row=6,sticky="SW",pady=30)
 
 
 ## Profile Page
 frm_profile = ttk.Frame(root)
-lbl_nameProfile = ttk.Label(frm_profile, text="Nimi\nKoulu", font=30)
-btn_profile = ttk.Button(frm_profile, text="Profile")
-lbl_omatTiedot= ttk.Label(frm_profile, text="OMAT TIEDOT", font =40)
-lbl_tiedot = ttk.Label(frm_profile,text="Opettaja\nPuhelinnumero\nHuoltaja", font=30)
-btn_kuva = ttk.Button(frm_profile,image=img_schedule)
-btn_sposti = ttk.Button(frm_profile,text="Sposti")
-btn_salasana = ttk.Button(frm_profile,text="Vaihda salasana")
-btn_uloskirjautuminen = ttk.Button(frm_profile, text="Kirjaudu ulos", command=profileToLogin)
+lbl_nameProfile = ttk.Label(frm_profile, text="Nimi\nKoulu", font=('Lato', 25))
+btn_profile = tk.Button(frm_profile, image=img_profile, highlightthickness=0, bd=0, command=homeworkToProfile)
+lbl_omatTiedot= ttk.Label(frm_profile, text="OMAT TIEDOT", font=('Lato', 25))
+lbl_tiedot = ttk.Label(frm_profile, text="Opettaja\nPuhelinnumero\nHuoltaja", font=('Lato', 18))
+img_profileBigger = tk.PhotoImage(file="pictures/profileBigger.png")
+frm_picEmailPassword = ttk.Frame(frm_profile)
+frm_picEmailPassword.columnconfigure(0, weight=1)
+frm_picEmailPassword.columnconfigure(1, weight=2)
+btn_kuva = tk.Button(frm_picEmailPassword, image=img_profileBigger, highlightthickness = 0, bd = 0)
+btn_sposti = ttk.Button(frm_picEmailPassword, text="Sähköposti", width=13, style=ttk.Style().configure('TButton', font=('Lato', 18)), state=tk.DISABLED)
+btn_salasana = ttk.Button(frm_picEmailPassword, text="Vaihda salasana", width=13, style=ttk.Style().configure('TButton', font=('Lato', 18)), state=tk.DISABLED)
+btn_uloskirjautuminen = ttk.Button(frm_profile, text="Kirjaudu ulos", style=ttk.Style().configure('TButton', font=('Lato', 18)), command=profileToLogin)
 img_settings = tk.PhotoImage(file="pictures/setting.png")
-btn_asetukset = ttk.Button(frm_profile,image=img_settings)
-btn_returnToMain = ttk.Button(frm_profile,text="Back", command=profileToMain)
+btn_asetukset = tk.Button(frm_profile, image=img_settings, highlightthickness = 0, bd = 0)
+btn_returnToMain = tk.Button(frm_profile, image=img_back, highlightthickness = 0, bd = 0, command=profileToMain)
 
-lbl_nameProfile.grid(row=0,column=0,sticky="NW", pady=20)
-btn_profile.grid(row=0, column=1,sticky="NE", pady=20)
-lbl_omatTiedot.grid(row=1)
-btn_kuva.grid(row=2,column=0,sticky="W")
-btn_sposti.grid(row=2, column=1,sticky="N")
-btn_salasana.grid(row=2,column=1,sticky="S")
-btn_uloskirjautuminen.grid(row=3,sticky="W")
-lbl_tiedot.grid(row=4,sticky="W")
-btn_asetukset.grid(row=5,sticky="W")
-btn_returnToMain.grid(row=6,sticky="W")
+lbl_nameProfile.grid(row=0, column=0, sticky="NW", pady=20)
+btn_profile.grid(row=0, column=1, sticky="E", pady=20)
+lbl_omatTiedot.grid(row=1, column=0, columnspan=2, sticky="W")
+frm_picEmailPassword.grid(row=2, column=0, columnspan=2, ipadx=2)
+btn_kuva.grid(row=0, column=0, rowspan=2)
+btn_sposti.grid(row=0, column=1, sticky="NE")
+btn_salasana.grid(row=1, column=1, sticky="SE")
+btn_uloskirjautuminen.grid(row=3, column=0, sticky="WE", columnspan=2, pady=2)
+lbl_tiedot.grid(row=4, column=0, sticky="W", columnspan=2)
+btn_asetukset.grid(row=5, column=0, sticky="SNEW", columnspan=2, pady=40)
+btn_returnToMain.grid(row=6, sticky="W")
 
 # Pack the Login page and start
 frm_login.pack()
